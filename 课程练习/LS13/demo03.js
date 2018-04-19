d118fe30ad5bff5e82b1cb1c10e52df4b06e3b99
moonhae <1137603852@qq.com>
2018-04-19 23:01:45 +0800

    4/19

--- 课程练习/LS13/demo03.js
+++ 课程练习/LS13/demo03.js
@@ -11,9 +11,12 @@ var obj3 = {};
 for(var i=0;i<10;i++){
     obj3.i = i;
 }
-console.log(obj4[0]);
+console.log(obj3[0]);
 //undefined
 //其他的也类似
+//这种加点的是相当于在obj3中加入了一个属性i，其值赋为了9
+console.log(obj3);
+//{i:9}
 
 var obj4 = {};
 for(var i=0;i<10;i++){
@@ -39,3 +42,8 @@ console.log(obj4[8]);
 //8
 console.log(obj4[9]);
 //9
+
+console.log(obj4);
+//{0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
+//这个加[]相当于给obj4中加入了10个属性，每个的值分别为0、1、2、3、4、5、6、7、8、9
+//综上所述，在使用for循环给对象中填入多个元素时，应该使用[]
