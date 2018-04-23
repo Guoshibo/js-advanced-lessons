2c7c73a7f702d03e96df7e8768167262c4382925
moonhae <1137603852@qq.com>
2018-04-23 14:37:35 +0800

    4.23

--- 课程练习/LS13/demo04.js
+++ 课程练习/LS13/demo04.js
@@ -15,7 +15,7 @@ console.log(obj.__proto__ === Object.prototype);
 
 //通过Object工场方法创建JS对象,注：JS对象是通过原型链的方式实现的对象继承
 var newObj = Object.create(obj);//newObj的原型是obj
-newObj.age = 23;
+newObj.age = 23;//给他提供了一个自身的属性
 console.log(newObj.num);
 console.log(newObj.str);
 console.log(newObj.show());
@@ -23,16 +23,31 @@ console.log(newObj.age);//自有属性
 console.log(newObj.__proto__);
 console.log(newObj.__proto__ === obj);
 
+var empty = {};
+var obj2 = Object.create(empty,{
+   x:{value:1}, y:{value:2,enumerable:true}
+});
+console.log(obj2);
+//{x:1,y:2}
+console.log(obj2.hasOwnProperty("x"));
+//true
+for(var k in obj2)
+{
+    console.log(k,obj2[k]);
+}
+//y 2
+
 //构造函数的方式创建JS对象  此处略讲，详情参照后续面向对象编程 注：JS对象是通过原型链的方式实现的对象继承
 function Person(name,age){
     this.name = name;
     this.age = age;
-}
+}//也可以给这个构造函数起个别名，这个构造函数充当了类
 Person.prototype.sayName = function(){
     console.log("hello,i'm",this.name,this.age,"years old");
 };
 var person1 = new Person("Mike",21);
 person1.sayName();
+//可以通过构造函数实例化出多个对象来
 
 //注意：
 var objStr = new Object("xxx");
