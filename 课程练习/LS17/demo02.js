d6e1899c7d59d5f37f19741b3e5833dd6a7b8dc9
moonhae <1137603852@qq.com>
2018-05-01 10:48:53 +0800

    5.1

--- 课程练习/LS17/demo02.js
+++ 课程练习/LS17/demo02.js
@@ -12,3 +12,20 @@ function Student(name,age,id){
 Student.prototype.__proto__ = Person.prototype;
 var s1 = new Student("xxx",22,2017001);
 var s2 = new Student("www",23,2017002);
+
+//JS实现继承的形式 二
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){
+    console.log(this.name);
+};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+var s1 = new Student("xxx",22,2017001);
+var s2 = new Student("www",23,2017002);
