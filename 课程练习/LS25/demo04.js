85df6495500f04866918d8309e01a5b17492b4ba
moonhae <1137603852@qq.com>
2018-05-28 14:54:15 +0800

    5.28

---
+++ 课程练习/LS25/demo04.js
@@ -0,0 +1,24 @@
+window.onload = function (e) {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+
+    div1.addEventListener("click",function (e) {
+        console.log("div1 click");
+    },false);//第3个参数可以不写，默认为false
+
+    div2.addEventListener("click",function (e) {
+        console.log("div2 click");
+    },false);
+
+    document.addEventListener("click",function (e) {
+        console.log("document click");
+    },false);
+
+    document.body.addEventListener("click",function (e) {
+        console.log("body click");
+    },false);
+}
+//b 1 2 d
+//d b 2 1
+//b 2 1 d
+
